35a87ab304c58b472713129e44e5901738642eb0
Juan Diego Acosta <juandiegoat01@gmail.com>
2020-04-23 19:33:42 -0500

    Adding a navbar

--- src/index.js
+++ src/index.js
@@ -6,11 +6,16 @@ import List from './containers/List'
 
 const App = () => {
     return (
+        <>
+        <nav className="navbar navbark-dark bg-dark border-bottom border-white">
+            <a href="/" className="navbar-brand">Movie App</a>
+        </nav>
         <main className="bg-dark" >
             <div className="container">
              <List/>
             </div>
         </main>
+        </>
     )
 }
 
